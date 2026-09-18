01620d80ff9ebf5d3459742f745d7b9d750ca5e4
priyanshitomar1310 <priyanshitomar1506@gmail.com>
2026-09-18 14:26:40 +0530

    Add files via upload

---
+++ MySpringBootProgram2/src/main/java/in/pt/main/controller/MyController.java
@@ -0,0 +1,17 @@
+package in.pt.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyController 
+{
+	@GetMapping("/")
+	@ResponseBody
+	public String printMsg()
+	{
+		return "Hello Priyanshi!";
+	}
+}
+ 
---
+++ MySpringBootProgram2/src/test/java/in/pt/main/MySpringBootProgram2ApplicationTests.java
@@ -0,0 +1,13 @@
+package in.pt.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootProgram2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
